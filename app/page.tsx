3de672bd8d54e262cdd90b454da9af9cbbe0b8d3
dragan039 <dragan039@gmail.com>
2026-08-23 18:23:51 +0200

    Izmenjeni nazivi sajta

--- app/page.tsx
+++ app/page.tsx
@@ -2671,6 +2671,8 @@ if (!loggedUser) {
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white border rounded p-6 w-full max-w-sm shadow">
         <h1 className="text-2xl font-bold mb-4 text-center">
+          PVC Kalkulator
+          <br/>
           Login
         </h1>
 
