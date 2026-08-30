0a1ebc876f6157f1c56cc9614a8bba479905058b
dragan039 <dragan039@gmail.com>
2026-08-30 11:45:55 +0200

    Izmenjeni nazivi sajta

--- app/page.tsx
+++ app/page.tsx
@@ -8732,15 +8732,20 @@ onChange={(e) => setSelectedProfilId(e.target.value)}
   </button>
 
 
-      <table className="border-collapse text-sm min-w-[900px]">
+      <div className="overflow-auto border max-h-[70vh]">
+  <table className="border-collapse text-sm min-w-[900px]">
 <thead className="bg-gray-200">
   <tr>
-    <th className="border p-2 min-w-[260px]">
+    <th className="border p-2 min-w-[220px] sticky top-0 left-0 z-30 bg-gray-200">
       Tekst na site-u
     </th>
 
+
     {languages.map((lang, index) => (
-      <th key={index} className="border p-2 min-w-[180px]">
+      <th
+  key={index}
+  className="border p-2 min-w-[180px] sticky top-0 z-20 bg-gray-200"
+>
         <input
           className="border p-1 w-full mb-1"
           value={lang.name || ""}
@@ -8907,7 +8912,7 @@ onChange={(e) => setSelectedProfilId(e.target.value)}
 
               {group.items.map((key) => (
                 <tr key={key}>
-                  <td className="border p-2 font-semibold bg-gray-50">
+                  <td className="border p-2 font-semibold bg-gray-50 sticky left-0 z-10">
                     {key}
                   </td>
 
@@ -8937,7 +8942,7 @@ onChange={(e) => setSelectedProfilId(e.target.value)}
         </tbody>
       </table>
     </div>
-    
+    </div>
 )}
 
 
